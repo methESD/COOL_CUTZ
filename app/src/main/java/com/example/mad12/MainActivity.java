bf38321c2dad7ca3e8cab5f28d6e21f9c7b89dc8
Meththani <meththaniranasinghe@gmail.com>
2020-08-17 00:49:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad12/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mad12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
